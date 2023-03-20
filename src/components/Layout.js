6c8f43681f092fd353b538df01071f5be1930437
techjuliana <julianasouzabittencourt@gmail.com>
2023-03-20 11:19:23 -0300

    aplicando estilo etc

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,7 +7,7 @@ export default function Layout({ children }) {
     <>
       <Head>
         <link rel="shortcut icon" href="/images/favicon.ico" />
-        <title>PokeNext</title>
+        <title>StarWars</title>
       </Head>
       <Navbar />
       <main className="main-container">{children}</main>
